66c653e4a1221019e15843a69b6204a827f6d3d8
Vladimir Kamarzin <vvk@vvk.pp.ru>
2021-01-18 23:04:14 +0500

    Bump to 0.4.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='birdfeeder',
-    version='0.4.0',
+    version='0.4.1',
     description='Helper library for CoinAlpha projects',
     python_requires='==3.*,>=3.6.1',
     project_urls={"repository": "https://github.com/coinalpha/birdfeeder"},
