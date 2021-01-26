c13d7005b7db19f062fdc7c7888a12ac37e23e01
Vladimir Kamarzin <vvk@vvk.pp.ru>
2021-01-26 13:44:52 +0500

    (feat) Add format_timestamp from parrot

--- birdfeeder/timestamps.py
+++ birdfeeder/timestamps.py
@@ -34,3 +34,42 @@ def timestamp_ms_to_str(ts: Timestamp_ms) -> str:
 def timestamp_s_to_str(ts: Timestamp_s) -> str:
     """Convert unixtime timestamp into human-readable form."""
     return f"{pd.Timestamp(ts, unit='s')}"
+
+
+def format_timestamp(timestamp: Union[int, float, str], to_format: str) -> Union[int, float]:
+    """
+    Convert timestamp from ms/ns/seconds to ms int / seconds int / seconds float form.
+
+    Caveats:
+    1. Input ms/ns timestamps are assumed to be fixed-length, which constraints operating range to 2001 - 2262
+       (64-bit C long).
+    2. Input timestamp cannot be in scientific notation!
+
+    :param timestamp: timestamp in float or int or str
+    :param to_format: "s" for second, "ms" for millisecond
+    :return: timestamp seconds or milliseconds
+    """
+    timestamp_as_str: str = str(timestamp)
+    milliseconds: int
+    seconds: float
+
+    if timestamp_as_str.isnumeric() and len(timestamp_as_str) == 13:  # ms in int
+        milliseconds = int(timestamp_as_str)
+        seconds = int(timestamp_as_str) / 1000
+    elif timestamp_as_str.isnumeric() and len(timestamp_as_str) == 19:  # ns in int
+        milliseconds = int(int(timestamp_as_str) / 1e6)
+        seconds = int(int(timestamp_as_str) / 1e9)
+    elif (
+        timestamp_as_str.replace(".", "", 1).isdigit() and len(timestamp_as_str.split(".")[0]) == 10
+    ):  # second in float
+        milliseconds = int(float(timestamp_as_str) * 1000)
+        seconds = float(timestamp_as_str)
+    else:
+        raise ValueError(f"Invalid timestamp format: {timestamp} (len {len(timestamp_as_str)})")
+
+    if to_format == "ms":
+        return milliseconds
+    elif to_format == "s":
+        return seconds
+    else:
+        raise ValueError("Invalid param to_format expecting [s, ms]")
--- tests/test_timestamps.py
+++ tests/test_timestamps.py
@@ -2,8 +2,15 @@ import time
 from unittest.mock import patch
 
 import pandas as pd
+import pytest
 
-from birdfeeder.timestamps import get_current_timestamp, pd_ts_to_timestamp_ms, timestamp_ms_to_str, timestamp_s_to_str
+from birdfeeder.timestamps import (
+    format_timestamp,
+    get_current_timestamp,
+    pd_ts_to_timestamp_ms,
+    timestamp_ms_to_str,
+    timestamp_s_to_str,
+)
 from birdfeeder.typing_local import Timestamp_ms
 
 
@@ -33,3 +40,43 @@ def test_timestamp_s_to_str():
     ts = 1579046400
     human_readable = timestamp_s_to_str(ts)
     assert human_readable == '2020-01-15 00:00:00'
+
+
+def test_format_timestamp_input_ms_int():
+    ts = get_current_timestamp()
+
+    result = format_timestamp(ts, "ms")
+    assert result == ts
+
+    result = format_timestamp(ts, "s")
+    assert result == ts / 1000
+
+
+def test_format_timestamp_input_ns_int():
+    ts = time.time_ns()
+
+    result = format_timestamp(ts, "ms")
+    expected = int(ts * 10 ** -6)
+    assert result == expected
+
+    result = format_timestamp(ts, "s")
+    expected = int(ts * 10 ** -9)
+    assert result == expected
+
+
+def test_format_timestamp_input_seconds_float():
+    ts = time.time()
+    result = format_timestamp(ts, "ms")
+    assert result == int(ts * 1e3)
+
+    result = format_timestamp(ts, "s")
+    assert result == ts
+
+
+def test_format_timestamp_invalid_input():
+    with pytest.raises(ValueError, match="Invalid timestamp format"):
+        format_timestamp(1e6, "ms")
+    with pytest.raises(ValueError, match="Invalid timestamp format"):
+        format_timestamp("foo", "ms")
+    with pytest.raises(ValueError, match="Invalid timestamp format"):
+        format_timestamp(42, "s")
