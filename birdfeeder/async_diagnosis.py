0186f4c8d4c0530f09582e19ffb6e3430f09f3b7
Vladimir Kamarzin <vvk@vvk.pp.ru>
2022-05-05 16:49:41 +0500

    (refactor) Fix typing issues in async_diagnosis.py

--- birdfeeder/async_diagnosis.py
+++ birdfeeder/async_diagnosis.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python
-
 """Debug console diagnosis tools."""
 
 import asyncio
-from typing import Coroutine, Generator, List, Union
+from typing import Any, Awaitable, Generator, List, Union
 
 import pandas as pd
 
 
-def get_coro_name(coro: Union[Coroutine, Generator]) -> str:
+def get_coro_name(coro: Any) -> str:
     if hasattr(coro, '__qualname__') and coro.__qualname__:
         coro_name = coro.__qualname__
     elif hasattr(coro, '__name__') and coro.__name__:
@@ -18,16 +16,18 @@ def get_coro_name(coro: Union[Coroutine, Generator]) -> str:
     return f'{coro_name}()'
 
 
-def get_wrapped_coroutine(task: asyncio.Task) -> Union[Coroutine, Generator]:
+def get_wrapped_coroutine(task: asyncio.Task) -> Union[Awaitable, Generator]:
     if "safe_wrapper" in str(task):
-        return task.get_coro().cr_frame.f_locals["c"]
+        # This is a backward compatibility logic when safe_ensure_future didn't adjust __qualname__
+        # TODO (vladimir): looks like some get_coro() return types may not have cr_frame attribute
+        return task.get_coro().cr_frame.f_locals["c"]  # type: ignore[union-attr]
     else:
         return task.get_coro()
 
 
 def active_tasks() -> pd.DataFrame:
     tasks: List[asyncio.Task] = [t for t in asyncio.Task.all_tasks() if not t.done()]
-    coroutines: List[Union[Coroutine, Generator]] = [get_wrapped_coroutine(t) for t in tasks]
+    coroutines = [get_wrapped_coroutine(t) for t in tasks]
     func_names: List[str] = [get_coro_name(c) for c in coroutines]
     retval: pd.DataFrame = pd.DataFrame(
         [{"func_name": f, "coroutine": c, "task": t} for f, c, t in zip(func_names, coroutines, tasks)],
